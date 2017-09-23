450473f64ac3b080c6a4b52c3a98f18b14797ee3
AlexKhotian <doctaww@gmail.com>
2017-09-23 21:28:40 +0200

    Added TCP conn

--- Shared/Networking/TCPClient.go
+++ Shared/Networking/TCPClient.go
@@ -1,5 +1,27 @@
 package Networking
 
+import (
+	"log"
+	"net"
+	"strconv"
+)
+
 // TCPClient creates connection with monitoring server, holds main connection
 type TCPClient struct {
+	port    int32
+	address *string
+}
+
+func CreateTCPClient(port int32, address *string) *TCPClient {
+	return &TCPClient{port, address}
+}
+
+func (client *TCPClient) Run() bool {
+	conn, err := net.Dial("tcp", *client.address+":"+strconv.Itoa(int(client.port)))
+	if err != nil {
+		log.Println("Failed to connect with error:", err.Error())
+		return false
+	}
+
+	return true
 }
--- Shared/Networking/TCPServer.go
+++ Shared/Networking/TCPServer.go
@@ -1,13 +1,41 @@
 package Networking
 
+import (
+	"log"
+	"net"
+	"strconv"
+)
+
 // TCPServer creates and handles listener, later forwards data
 type TCPServer struct {
 	port int32
 }
 
+// CreateTCPServerOnPort creating a server on the given port
+func CreateTCPServerOnPort(port int32) *TCPServer {
+	server := new(TCPServer)
+	server.port = port
+	return server
+}
+
 // Run start a server and wait for incoming connection
 func (server *TCPServer) Run() bool {
+	portString := ":" + strconv.Itoa(int(server.port))
+	listener, err := net.Listen("tcp", portString)
+	if err != nil {
+		log.Println("Failed to listen with error:", err.Error())
+		return false
+	}
+	conn, err := listener.Accept()
+	if err != nil {
+		log.Println("Failed to accept with error:", err.Error())
+		return false
+	}
+	server.handleConnection(&conn)
 	return true
+}
+
+func (server *TCPServer) handleConnection(conn *net.Conn) {
 }
 
 // Shutdown server and clean up resources
